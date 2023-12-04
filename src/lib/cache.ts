f03d3e7e38aa0a2888a857f57eb5b064a205dd06
malakai <whomeverrblx@gmail.com>
2023-12-04 14:21:18 +0000

    improve cache

--- src/lib/cache.ts
+++ src/lib/cache.ts
@@ -1,18 +1,12 @@
 const MAX_CACHE_SIZE: number = 1000;
 
-const hash = (key: string): number => {
-    return key.split("").reduce((a: number, b: string) => {
-        a = (a << 5) - a + b.charCodeAt(0);
-        return a & a;
-    }, 0);
-};
+type Cacheable =
+    | {
+          id: string;
+      }
+    | string;
 
-type Cacheable = {
-    id: string;
-};
-type AcceptableInput = Cacheable | string;
-
-function getSnowflake(key: AcceptableInput): string {
+function getSnowflake(key: Cacheable): string {
     if (typeof key === "string") {
         return key;
     } else {
@@ -21,43 +15,40 @@ function getSnowflake(key: AcceptableInput): string {
 }
 
 export class AsyncCache<T extends Cacheable> {
-    private cache: Map<number, any>;
+    private cache: Map<string, T>;
 
     constructor() {
         this.cache = new Map();
     }
 
-    async create(key: AcceptableInput): Promise<T> {
+    async create(key: Cacheable): Promise<T> {
         throw new Error("Cache#create: Not implemented");
     }
 
-    async get(key: AcceptableInput): Promise<T> {
+    get(key: Cacheable): Promise<T> {
         const id = getSnowflake(key);
-        const cached = this.cache.get(hash(id));
+        const cached = this.cache.get(id);
 
         if (cached) {
-            return cached;
+            return Promise.resolve(cached);
         }
 
-        return await this.create(id);
+        return this.create(key);
     }
 
-    get_raw(key: AcceptableInput): T {
-        const id: number = hash(getSnowflake(key));
-        return this.cache.get(id);
+    get_raw(key: Cacheable): T | undefined {
+        return this.cache.get(getSnowflake(key));
     }
 
-    set(key: AcceptableInput, value: T): void {
-        const id: number = hash(getSnowflake(key));
+    set(key: Cacheable, value: T): void {
         if (this.cache.size >= MAX_CACHE_SIZE) {
             this.cache.delete(this.cache.keys().next().value);
         }
-        this.cache.set(id, value);
+        this.cache.set(getSnowflake(key), value);
     }
 
-    delete(key: AcceptableInput): boolean {
-        const id: number = hash(getSnowflake(key));
-        return this.cache.delete(id);
+    delete(key: Cacheable): boolean {
+        return this.cache.delete(getSnowflake(key));
     }
 
     clear(): void {
